545e4c637836080465d8c2818fac9c5d3e19ddf1
kkaplan <kkaplan@usra.edu>
2020-06-08 14:12:44 -0700

    Added raster mapping.

--- sens_map_lib.py
+++ sens_map_lib.py
@@ -8,6 +8,11 @@ import numpy as np
 from astropy.io import fits
 from astropy.modeling import models, fitting
 
+
+
+
+
+
 #Class that stores the 2D array representing the sky and it's associated coordinate system
 class sky:
 	def __init__(self, max_x, max_y, plate_scale):
@@ -34,19 +39,22 @@ class GREAT_array:
 		self.array_profile = None #Holder for array profile
 		self.angle = 0.
 	def position(self, x, y): #Move center of array to (x,y)
+		self.reset_position() #First zero position
 		for pixel in self.array_profile:
 			pixel.x_mean = pixel.x_mean + x
 			pixel.y_mean = pixel.y_mean + y
-	def zero_position(self): #Rezeros position around the 0th pixel
+	def reset_position(self): #Rezeros position around the 0th pixel
 		zeroth_pixel = self.array_profile[0]
 		dx = zeroth_pixel.x_mean.value
 		dy = zeroth_pixel.y_mean.value
 		for pixel in self.array_profile: #Move back position zero point
 			pixel.x_mean = pixel.x_mean - dx
 			pixel.y_mean = pixel.y_mean - dy
-		self.rotate(-self.angle) #Rotate back to angle zero point
 		self.angle = 0.
-	def rotate(self, angle): #Rotate the positions of all the pixels around the 0th pixel (angle is inputted in degrees)
+	def rotate(self, angle): #Rotate array by the given angle
+		self.reset_array_rotation() #Zero rotation angle by reversing the previous rotation
+		self.set_array_rotation(angle) #Set new rotation angle
+	def set_array_rotation(self, angle): #Rotate the positions of all the pixels around the 0th pixel (angle is inputted in degrees)
 		zeroth_pixel = self.array_profile[0] #Grab central pixel
 		zero_point = np.array([zeroth_pixel.x_mean, zeroth_pixel.y_mean]).T #Store zero point of central pixel as a 
 		c, s = np.cos(np.radians(angle)), np.sin(np.radians(angle)) #Construct rotation matrix
@@ -58,17 +66,30 @@ class GREAT_array:
 			pixel.x_mean = new_position[0]
 			pixel.y_mean = new_position[1]
 		self.angle += angle #Store overall angle of rotation
-	def reset_rotation(self): #Rotate back to an angle of zero
-		self.rotate(-self.angle)
-	def paint(self, skyobj, time=1.0): #Paint a single instance of the array profile onto a sky object, this is the base for all observation types including single pointing and maps
-		skyobj.data += self.array_profile(skyobj.x, skyobj.y) * time
-	def single_point(self, skyobj, x=0., y=0., time=1.0, angle=0.): #Paint a single point observation onto the sky object
-		self.zero_position() #First zero position
-		self.rotate(angle) #Set rotation angle
+	def reset_array_rotation(self): #Rotate back to an angle of zero
+		self.set_array_rotation(-self.angle)
+	def paint(self, skyobj, time=1.0, cycles=1): #Paint a single instance of the array profile onto a sky object, this is the base for all observation types including single pointing and maps
+		skyobj.data += self.array_profile(skyobj.x, skyobj.y) * time * cycles
+	def single_point(self, skyobj, x=0., y=0., time=1.0, array_angle=0., cycles=1): #Paint a single point observation onto the sky object	
+		self.rotate(array_angle) #Set rotation angle
 		self.position(x, y) #Set central position for the single pointing
-		self.paint(skyobj, time=time) #Paint the single pointing to the sky object
-
-
+		self.paint(skyobj, time=time, cycles=cycles) #Paint the single pointing to the sky object
+	def raster(self, skyobj, x=0., y=0., nx=1, ny=1, dx=1.0, dy=1.0, time=1.0, cycles=1, array_angle=0., map_angle=0.):
+		self.rotate(array_angle) #Set rotation angle
+		map_y, map_x = np.mgrid[0:ny,0:nx] #Generate map coordinates
+		map_x = map_x * dx
+		map_y = map_y * dy
+		cos_map_angle = np.cos(np.radians(map_angle)) #Rotate map coordiunates by map angle (using a rotation matrix) and add starting position to map coordinates
+		sin_map_angle = np.sin(np.radians(map_angle))
+		rotated_map_x = x + (cos_map_angle*map_x + sin_map_angle*map_y)
+		rotated_map_y = y + (-sin_map_angle*map_x + cos_map_angle*map_y)
+		for ix in range(nx): #Loop through each step in the map
+			for iy in range(ny):
+			# 	print('x=', map_x[iy,ix])
+			# 	print('y=', map_y[iy,ix])
+				self.position(rotated_map_x[iy,ix], rotated_map_y[iy,ix]) #Set array position at this step
+				self.paint(skyobj, time=time, cycles=cycles) #Paint the current step to the sky object
+				
 
 #Child class to store array profile for the LFA
 class LFA_array(GREAT_array):
